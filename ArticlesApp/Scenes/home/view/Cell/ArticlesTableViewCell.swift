2bb24b70dbf708bb387d4e71c9b85c73877124d8
Ahmed Gado <ahmed_gado2001@yahoo.com>
2022-03-05 10:38:11 +0200

    add ArticlesTableViewCell

---
+++ ArticlesApp/Scenes/home/view/Cell/ArticlesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ArticlesTableViewCell.swift
+//  ArticlesApp
+//
+//  Created by ahmed gado on 05/03/2022.
+//
+
+import UIKit
+
+class ArticlesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
